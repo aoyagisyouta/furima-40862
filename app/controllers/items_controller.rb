122321aac49e664c50a79b7688c81106a9b5b164
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-07-21 17:13:22 +0900

    Active Storage導入及びバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end 
   def create 
+    @item = Item.create(item_params)
   end 
+
+
+  private 
+  def item_params 
+    params.require(:item).permit(:item_name, :item_info, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image 
+
+  with_options presence: true do 
+    validates :item_name
+    validates :item_info
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+    validates :user
+  end 
+  validates :image, presence: true, unless: :was_attached?
+
+  def was_attached? 
+    self.image.attached? 
+  end 
+  
 end

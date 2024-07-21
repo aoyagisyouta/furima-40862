1143fc6282c4d3ba26c0779bbd016370f2336a1c
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-07-21 21:44:49 +0900

    商品保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,14 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end 
   end 
-  def create 
-    @item = Item.create(item_params)
-  end 
-
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private 
   def item_params 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-  belongs_to :shipping_cost
-  belongs_to :shipping_day
-  belongs_to :item_status
+  belongs_to :shipping_cost, class_name: 'ShippingCost'
+  belongs_to :shipping_day, class_name: 'ShippingDay'
+  belongs_to :item_status, class_name: 'ItemStatus'
   belongs_to :category
   belongs_to :user
   has_one_attached :image 

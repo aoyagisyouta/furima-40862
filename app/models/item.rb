49172d6ec9dac7f21ab2630e65323bb60fece800
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-07-21 20:14:36 +0900

    各モデルでActiveHash記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
+  belongs_to :item_status
   belongs_to :category
   belongs_to :user
   has_one_attached :image 

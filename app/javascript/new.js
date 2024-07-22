2051c3af0896bfeec009be4f60fd2b76f466ce7d
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-07-22 21:52:27 +0900

    販売手数料・販売利益表示

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,4 +1,13 @@
-window.addEventListener('load', function(){
-  const taxPrice = this.document.getElementById('add-tax-price')
-  console.log(taxPrice)
-})
+function item (){
+  const itemPrice = document.getElementById('item-price');
+  itemPrice.addEventListener("keyup", () => {
+    const tax = Math.floor(itemPrice.value / 10);
+    const addTaxPrice = document.getElementById('add-tax-price');
+    const profitPrice = Math.floor(itemPrice.value - tax);
+    const Profit = document.getElementById('profit');
+    addTaxPrice.innerHTML = `${tax}`;
+    Profit.innerHTML = `${profitPrice}`;
+  });
+ };
+ 
+ window.addEventListener('turbo:load', item);

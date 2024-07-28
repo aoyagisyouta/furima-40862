941c7530c0ba1791919770e5e304d84d2c75520e
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-07-28 21:43:08 +0900

    購入画面ビュー編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
   get 'purchases/index'
   devise_for :users
   root to: 'items#index'
-<<<<<<< HEAD
+
   resources :items do
     resources :purchases, only: [:index,:create]
   end
   resources :purchase_addresses
-=======
-  resources :items do 
-    resources :purchases, only: [:index, :create]
-  end
->>>>>>> d31ad30df2a9ca5b3e8e10d7d46b3c6b4ee87864
 end 
